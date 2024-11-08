285c86446ebea629d3cf801b6b73f30a0ac47286
Yakov Karpov <yakovkarpoff@gmail.com>
2024-11-08 02:16:38 +0200

    Update template

--- template/flake.nix
+++ template/flake.nix
@@ -2,26 +2,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     core-flake = {
-      url = "github:purplenoodlesoop/core-flake/master";
+      url = "github:purplenoodlesoop/core-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
-  outputs = { self, nixpkgs, core-flake }:
-    let
-      core = core-flake.lib;
-      name = throw "Undefined flake name";
-      systemSpecific = { pkgs, toolchains, system }:
-        let
-
-        in
-        {
-          # shells = {};
-          # apps = {};
-          # packages = {};
-        };
-    in
-    core.mkFlake {
-      inherit name systemSpecific;
+  outputs =
+    inputs:
+    inputs.core-flake.lib.evalFlake {
+      imports = [
+        ./.
+      ];
     };
 }
